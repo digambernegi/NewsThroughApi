333c5464a63ffd80dd44cd447bcc24b0be1e0428
digambernegi <digambernegi001@gmail.com>
2021-11-18 19:37:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const[input,setInput] = useState('Technology');
+  const [article,setArticle]=useState([])
+
+  function readValue(value){
+    setInput(value);
+  }
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
+  useEffect(() => {
+      getNews();
+  }, [input])
+
+  function getNews(){
+    fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=88b5ed72a3e543f9a116a1633f92885e`)
+    .then((response)=> response.json())
+    .then((data)=>{
+      setArticle(data.articles);
+      console.log(data.articles)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <h1>News</h1>
+    <div className="search">
+    <input type="text" onChange={(e) =>{readValue(e.target.value)}} placeholder="category"/>
+    <button onClick={getNews}>Submit</button>
+    </div>
+    
+   <div className="wrapper">
+   {
+     article.map((articles,index) =>{
+       return(
+         <div key={index} className="cards">
+           <img src={articles.urlToImage} alt="img-src" />
+           <h3>{articles.title}</h3>
+           
+           <div className="author">
+           <p>{articles.author}</p>
+           <p className="article__Time">{articles.publishedAt}</p>
+           </div>
+           <p>{truncate(articles.content,200)}</p>
+           <a href={articles.url}>
+           <button className="more__btn">Read More</button>
+           </a>
+         </div>
+       )
+     })
+   }  
+   
+   </div>
     </div>
   );
 }
